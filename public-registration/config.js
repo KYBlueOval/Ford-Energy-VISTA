1c9d5f2339bf86b2346acd10ccbcacd056bc88c5
Ford Energy - BlueOval Glendale <ky1.blueovalsk@gmail.com>
2026-07-14 12:39:38 -0400

    Update Registration for Sec/Safe Video

--- public-registration/config.js
+++ public-registration/config.js
@@ -1,5 +1,6 @@
 window.FE_VISITOR_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxJhQ9P6V8Un4oyZEy5JNp4ravCRbfKm1Dvbay87t_pydQmwNhOMJfxIBmAlBMNoMLQ/exec",
+  TRAINING_VIDEO_URL: "https://drive.google.com/file/d/1ARmWVohP6JvpK6rZFu-QK10yii8sS3TT/view?usp=sharing",
   SITE_NAME: "Ford Energy Glendale",
   AGREEMENT_VERSION: "2026.1"
 };
