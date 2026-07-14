1a22735cafd82d49ea6cc84bdd88c8da68cc825e
Ford Energy - BlueOval Glendale <ky1.blueovalsk@gmail.com>
2026-07-14 13:40:00 -0400

    Security-Safety Video Update

--- public-registration/config.js
+++ public-registration/config.js
@@ -1,8 +1,8 @@
 window.FE_VISITOR_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxJhQ9P6V8Un4oyZEy5JNp4ravCRbfKm1Dvbay87t_pydQmwNhOMJfxIBmAlBMNoMLQ/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxJhQ9P6V8Un4oyZEy5JNp4ravCRbfKm1Dvbav87t_pydQmwNhOMJfxIBmA1BMNoMLQ/exec",
   SITE_NAME: "Ford Energy Glendale",
   AGREEMENT_VERSION: "2026.2",
   SECURITY_CONSOLE_URL: "https://kyblueoval.github.io/Ford-Energy-VISTA/security-console/",
-  VGS_NAVIGATION_URL: "",
-  TRAINING_VIDEO_URL: ""
+  VGS_NAVIGATION_URL: "https://kyblueoval.github.io/Ford-Energy-Visitor-Guidance-System/",
+  TRAINING_VIDEO_URL: "https://drive.google.com/file/d/1ARmWVohP6JvpK6rZFu-QK10yii8sS3TT/preview"
 };
